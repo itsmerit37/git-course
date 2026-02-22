62d8febd8590f7bce52f50d98817c07f5fb81d63
itsmerit37 <itsmerit37@gmail.com>
2026-02-22 22:39:14 +0530

    JDBC connectivity

--- YouTube/src/main/java/org/example/DemoClass.java
+++ YouTube/src/main/java/org/example/DemoClass.java
@@ -3,11 +3,20 @@ import java.sql.*;
 
 public class DemoClass {
     public static void main(String[] args) throws Exception{
-        String url="";
-        String uMame="root";
-        String password="";
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection con = DriverManager.getConnection(url,uMame,password);
+        String url="jdbc:mysql://localhost:3306/aliens";
+        String uName="root";
+        String password="root";
+        String query= "select name from student where userid=1";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(url,uName,password);
+        Statement st= con.createStatement();
+        ResultSet rs=st.executeQuery(query);
+
+        rs.next();
+        String name=rs.getString("name");
+        System.out.println(name);
+        st.close();
+        con.close();
 
 
 
